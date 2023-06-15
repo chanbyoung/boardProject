f9b681574325a7ae2bde3f7046bf324c267f84bd
park <pcb7893@naver.com>
2023-06-15 10:15:11 +0900

    Board Project

---
+++ src/main/java/firstProject/board/domain/post/PostRepositoryImpl.java
@@ -0,0 +1,49 @@
+package firstProject.board.domain.post;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class PostRepositoryImpl implements PostRepository {
+    private static final Map<Long, Post> store = new HashMap<>();
+    private static long sequence = 0L;
+    private static Long readCount =0L;
+    @Override
+    public Post save(Post post){
+        post.setId(++sequence);
+        post.setReadCount(readCount);
+        store.put(post.getId(), post);
+        return post;
+    }
+    @Override
+    public Map saveComment(Post post, Comment comment) {
+        Map<Long, Comment> commentList = post.getCommentList();
+        int size = commentList.size();
+        commentList.put((long) ++size, comment);
+        return commentList;
+    }
+    @Override
+    public Post findById(Long id){
+        return store.get(id);
+    }
+    @Override
+    public List<Post> findAll(){
+        return new ArrayList<>(store.values());
+    }
+    @Override
+    public void update(Long id, Post updateParam){
+        Post findPost = findById(id);
+        findPost.setPostName(updateParam.getPostName());
+        findPost.setContent(updateParam.getContent());
+    }
+    @Override
+    public void updateReadCount(Long id){
+        Post updatePost = findById(id);
+        updatePost.setReadCount(updatePost.getReadCount()+1);
+    }
+
+}
